aec997dd5a0a2835cb63d2590cb3f98d9fc4731d
klee8 <kate.d.lee@gmail.com>
2019-06-27 12:11:10 +1200

    rm setup files

--- E.fe_plant_vs_culture_DE/3_DESeq2/DE_setup.sh
+++
@@ -1,5 +0,0 @@
-mkdir E.festucae_Fl1_v3
-cp ../Generic_DESeq2.Rmd E.festucae_Fl1_v3/E.festucae_Fl1_v3.DESeq2.WT.3DP.Rmd
-sed -i "s/SPECIES/E.festucae_Fl1_v3/g" E.festucae_Fl1_v3/E.festucae_Fl1_v3.DESeq2.WT.3DP.Rmd
-sed -i "s/SOI/WT/g" E.festucae_Fl1_v3/E.festucae_Fl1_v3.DESeq2.WT.3DP.Rmd
-sed -i "s/REF/3DP/g" E.festucae_Fl1_v3/E.festucae_Fl1_v3.DESeq2.WT.3DP.Rmd
